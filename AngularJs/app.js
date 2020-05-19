790c108dd4ff8a72a871098e83af058abb5b047c
jeromehenry <jerome.95660@gmail.com>
2020-05-19 11:20:45 +0530

    Add files via upload

---
+++ AngularJs/app.js
@@ -0,0 +1,21 @@
+var app = angular.module('app', ['ui.router']);
+
+app.config(function($stateProvider,$urlRouterProvider){
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+    .state('/',{
+        url: '/',
+            templateUrl:'patient/patientDetails.html',
+            controller: 'patientsCtrl',
+    })
+    .state('medicine',{
+        url: '/medicine',
+            templateUrl:'medicine/medicineDetails.html',
+            
+    });
+})
+
+// app.controller('patientsCtrl',function($scope){
+//     $scope.patient = 'Jerome';
+// })
